77d92f48ced235d3dec9f5a0b713a5fba2cf607b
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2025-11-21 19:56:11 +0400

    modify next config to fix styled compknent render issue

--- Desktop/next-app/app/(dashboard)/page.tsx
+++ Desktop/next-app/app/(dashboard)/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import Styles from "../../styles/Home.module.css";
+import styled from "styled-components";
 
 export default function Home() {
-console.log(Styles)
   return (
     <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
       <h1 className={Styles.heading}>
         To get started, edit the page.tsx file.
       </h1>
+      <StyledHeading>This is styled component</StyledHeading>
     </main>
   );
 }
+
+const StyledHeading = styled.h2`
+  color: green;
+`;
